ec5bb5951cdc78cafc910ecd9390c4f82e9f0f08
Mehdy_Selem <101666052+Mehdisel23@users.noreply.github.com>
2025-08-24 10:32:06 +0200

    Delete coffe-house-front/coffehouse directory

--- coffe-house-front/coffehouse/caffe/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from caffe.models import Coffee
-
-# Register your models here.
-admin.site.register(Coffee)
--- coffe-house-front/coffehouse/userapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from caffe.models import Coffee
-from userapp.models import User, ClientHistory, Seller
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Seller)
-admin.site.register(ClientHistory)
